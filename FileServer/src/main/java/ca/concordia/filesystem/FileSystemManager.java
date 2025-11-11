9345feb32d307f978196f14e491bdd011107d22c
lucas-graham <saculiomaharg@gmail.com>
2025-11-10 23:54:20 -0500

    Add barebones initialization of FileSystemManager

--- FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
+++ FileServer/src/main/java/ca/concordia/filesystem/FileSystemManager.java
@@ -2,6 +2,7 @@ package ca.concordia.filesystem;
 
 import ca.concordia.filesystem.datastructures.FEntry;
 
+import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -9,8 +10,8 @@ public class FileSystemManager {
 
     private final int MAXFILES = 5;
     private final int MAXBLOCKS = 10;
-    private final static FileSystemManager instance;
-    private final RandomAccessFile disk;
+    private final static FileSystemManager instance = null;
+    private  RandomAccessFile disk = null;
     private final ReentrantLock globalLock = new ReentrantLock();
 
     private static final int BLOCK_SIZE = 128; // Example block size
@@ -21,7 +22,22 @@ public class FileSystemManager {
     public FileSystemManager(String filename, int totalSize) {
         // Initialize the file system manager with a file
         if(instance == null) {
+
+            try {
+
+                disk = new RandomAccessFile("filesystem", "rw");
+                disk.seek(0);
+                disk.writeUTF(filename);
+                disk.seek(10);
+                disk.writeInt(totalSize);
+
+
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
             //TODO Initialize the file system
+
         } else {
             throw new IllegalStateException("FileSystemManager is already initialized.");
         }
@@ -34,5 +50,24 @@ public class FileSystemManager {
     }
 
 
-    // TODO: Add readFile, writeFile and other required methods,
+    public void deleteFile(String fileName) throws Exception {
+        // TODO
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
+
+
+    public void writeFile(String fileName) throws Exception {
+        // TODO
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
+    
+    public byte[] readFile(String fileName) throws Exception {
+        // TODO
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
+
+    public String[] listFiles() throws Exception {
+        // TODO
+        throw new UnsupportedOperationException("Method not implemented yet.");
+    }
 }
